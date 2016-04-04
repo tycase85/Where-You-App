a60e466d1fd7d5571c14896c410379214092ec1c
rcase <tyler.case@live.com>
2016-04-04 03:44:06 -0300

    lll

---
+++ app/src/main/java/mobiledev/unb/ca/whereyouapp/FriendData.java
@@ -0,0 +1,17 @@
+package mobiledev.unb.ca.whereyouapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rcase on 03/04/16.
+ */
+public class FriendData {
+    public String email;
+    public String option;
+    public int imageId;
+
+    FriendData(String email){
+        this.email = email;
+    }
+}
